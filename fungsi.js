7476baeb39038a787ea4398822e5c6dc2568bfdd
Simple Playtime Game <boni.sena@ft.unsika.ac.id>
2026-04-12 21:30:08 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,4 +1,8 @@
 const add = (a, b) => a+b;
 console.log(add(1,2));
-const substract =(a,b) >= a-b;
+
+const substract =(a,b) => a-b;
 console.log(substract(5,2));
+
+const multiply =(a,b) => a*b;
+console.log(multiply(2,2));
