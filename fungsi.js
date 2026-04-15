57b356118f69515deea931f1f9b991b3932fd334
Simple Playtime Game <boni.sena@ft.unsika.ac.id>
2026-04-15 19:41:52 +0700

    Merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,10 @@
 const add = (a, b) => a+b;
 console.log(add(1,2));
-
-const substract =(a,b) => a-b;
+const substract =(a,b) >= a-b;
 console.log(substract(5,2));
 
-const multiply =(a,b) => a*b; 
-console.log(multiply(2,2));
+const multiplyNumber =(a,b) => a*b; 
+console.log(multiplyNumber(2,2));
 
 const divide =(a,b) => a/b; 
 console.log(divide(4/2));  
