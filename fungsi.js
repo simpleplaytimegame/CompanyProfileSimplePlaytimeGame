e5d9db01ef0313bc670f18207357ec6040597555
Simple Playtime Game <boni.sena@ft.unsika.ac.id>
2026-05-26 20:10:39 +0700

    menambahkan fungsi math sin

--- fungsi.js
+++ fungsi.js
@@ -1,13 +1,16 @@
-const add = (a, b) => a+b;
+const add = (a, b) => a + b;
 console.log(add(1,2));
-const substract =(a,b) >= a-b;
+
+const substract = (a,b) => a - b;
 console.log(substract(5,2));
 
-const multiplyNumber =(a,b) => a*b; 
+const multiplyNumber = (a,b) => a * b; 
 console.log(multiplyNumber(2,2));
 
-const divide =(a,b) => a/b; 
-console.log(divide(4/2));  
+const divide = (a,b) => a / b; 
+console.log(divide(4,2));  
 
 console.log(add(5,5));
 
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(10));
